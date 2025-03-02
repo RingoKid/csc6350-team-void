0ff0c4fa1d8b9ae707d62f52b7f38a4747113f3e
Kidus Tafa <tafa.kidusfekadu@gmail.com>
2025-03-02 17:24:05 -0500

    chore(models): fixed models and ran migrations

--- backend/api/admin.py
+++ backend/api/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
+from .models import Project, Feedback, Rating, Reaction, Collaboration, Notification, SearchLog, Report
 
-# Register your models here.
+admin.site.register(Project)
+admin.site.register(Feedback)
+admin.site.register(Rating)
+admin.site.register(Reaction)
+admin.site.register(Collaboration)
+admin.site.register(Notification)
+admin.site.register(SearchLog)
+admin.site.register(Report)
--- backend/api/models.py
+++ backend/api/models.py
@@ -1,5 +1,5 @@
 from django.db import models
-from django.contrib.auth.models import AbstractUser
+from django.contrib.auth.models import AbstractUser, Group, Permission
 
 # Custom User Model
 class User(AbstractUser):
@@ -12,6 +12,26 @@ class User(AbstractUser):
     role = models.CharField(max_length=10, choices=ROLE_CHOICES)
     profile_picture = models.ImageField(upload_to='profile_pics/', blank=True, null=True)
     institution = models.CharField(max_length=255, blank=True, null=True)
+
+    groups = models.ManyToManyField(
+        Group,
+        verbose_name=('groups'),
+        blank=True,
+        help_text=(
+            'The groups this user belongs to. A user will get all permissions '
+            'granted to each of their groups.'
+        ),
+        related_name="api_users_groups",  # Modified related_name
+        related_query_name="api_user", # Modified related_query_name
+    )
+    user_permissions = models.ManyToManyField(
+        Permission,
+        verbose_name=('user permissions'),
+        blank=True,
+        help_text=('Specific permissions for this user.'),
+        related_name="api_users_permissions", # Modified related_name
+        related_query_name="api_user", # Modified related_query_name
+    )
 
     def __str__(self):
         return self.username
