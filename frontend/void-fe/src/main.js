f103d1b8be73666d3d67d1c86e640cf4a9fed8de
ringo <rifat.naimul@gmail.com>
2025-03-06 20:34:36 -0500

    bootstrap integrated

--- frontend/void-fe/src/main.js
+++ frontend/void-fe/src/main.js
@@ -1,14 +1,27 @@
-import './assets/main.css'
-
 import { createApp } from 'vue'
-import { createPinia } from 'pinia'
-
 import App from './App.vue'
-import router from './router'
 
+// Import CSS files
+import './assets/plugins/bootstrap/bootstrap.min.css'
+import './assets/plugins/bootstrap/bootstrap-slider.css'
+import './assets/plugins/font-awesome/css/font-awesome.min.css'
+import './assets/plugins/slick/slick.css'
+import './assets/plugins/slick/slick-theme.css'
+import './assets/plugins/jquery-nice-select/css/nice-select.css'
+import './assets/css/style.css'
+
+// Create and mount the Vue app
 const app = createApp(App)
 
-app.use(createPinia())
-app.use(router)
-
 app.mount('#app')
+
+// Import JavaScript files (if necessary, or handle via components)
+import './assets/plugins/jquery/jquery.min.js'
+import './assets/plugins/bootstrap/popper.min.js'
+import './assets/plugins/bootstrap/bootstrap.min.js'
+import './assets/plugins/bootstrap/bootstrap-slider.js'
+import './assets/plugins/tether/js/tether.min.js'
+import './assets/plugins/raty/jquery.raty-fa.js'
+import './assets/plugins/slick/slick.min.js'
+import './assets/plugins/jquery-nice-select/js/jquery.nice-select.min.js'
+import './assets/js/script.js'
