1c4de89b9b06c6a11260ae93609d40a7ba4f6e5a
Kidus Tafa <tafa.kidusfekadu@gmail.com>
2025-03-25 09:28:26 -0400

    chore(tests):added usermodel unit test

--- backend/api/tests.py
+++ backend/api/tests.py
@@ -1,3 +1,15 @@
 from django.test import TestCase
+from .models import *
+class UserModelTest(TestCase):
+    def setUp(self):
+        self.user = User.objects.create_user(
+            username='testuser',
+            email='testuser@example.com',
+            password='password123',
+            role='Presenter'
+        )
 
-# Create your tests here.
+    def test_user_creation(self):
+        self.assertEqual(self.user.username, 'testuser')
+        self.assertEqual(self.user.email, 'testuser@example.com')
+        self.assertEqual(self.user.role, 'Presenter')
